7a1e206acefc536ad01ae43c4b974ee6d430e7e2
Your Name <gupta08gautam@gmail.com>
2026-05-12 02:55:59 +0530

    Practice

--- PracticeC/PatternsPrac/q9.c
+++ PracticeC/PatternsPrac/q9.c
@@ -1,3 +1,40 @@
-//
-// Created by gauta on 11-05-2026.
-//
+#include<stdio.h>
+
+int main() {
+    int n;
+    int m = 1;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for (int i = n; i > 0; i--) {
+        for (int j = 1; j < i; j++) {
+            printf(" ");
+        }
+        for (int k = 1; k <= (2 * m) - 1; k++) {
+            printf("*");
+        }
+        m++;
+        if (m >= n) {
+            break;
+        }
+        printf("\n");
+    }
+    printf("\n");
+    int o = n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            printf(" ");
+        }
+        for (int k = (2 * o) - 1; k > 0; k--) {
+            printf("*");
+        }
+        o--;
+        if (o == 0) {
+            break;
+        }
+        printf("\n");
+    }
+
+}
