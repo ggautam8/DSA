d3d3ca680e5eb0df3e56a24bf38c1bc370e1cd4e
Your Name <gupta08gautam@gmail.com>
2026-05-11 19:02:19 +0530

    Practice

--- PracticeC/PatternsPrac/q8.c
+++ PracticeC/PatternsPrac/q8.c
@@ -5,14 +5,20 @@ int main() {
 
     printf("Enter n: ");
     scanf("%d", &n);
+    int m = n;
 
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j <= i; j++) {
-            printf("* ");
+        for (int j = 0; j < i; j++) {
+            printf(" ");
         }
-        for (int k = (2 * n) - 1; k > 0; k--) {
-            printf(" *");
+        for (int k = (2 * m) - 1; k > 0; k = k - 1) {
+            printf("*");
         }
+        m--;
+        if (m == 0) {
+            break;
+        }
+
         printf("\n");
     }
 }
