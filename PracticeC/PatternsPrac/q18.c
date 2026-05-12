50d923cab5b5902a6830410dd98d7884ce9e121d
Your Name <gupta08gautam@gmail.com>
2026-05-13 02:08:44 +0530

    Practice

---
+++ PracticeC/PatternsPrac/q18.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = n - i; j <= n; j++) {
+            printf("%c ", (j+ 64));
+        }
+        printf("\n");
+    }
+}
