6d3a8b3c4624dc18b311e972113006d6d3f373f6
Your Name <gupta08gautam@gmail.com>
2026-06-24 18:25:04 +0530

    Practice

---
+++ LeetCode/Medium/ReverseInteger.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+    int n, rev = 0, rem = 0;
+
+    printf("Enter any integer: ");
+    scanf("%d", &n);
+
+    while (n != 0) {
+        rem = n % 10;
+        rev = rev * 10 + rem;
+        n = n / 10;
+    }
+
+    printf("\nReverse : %d", rev);
+}
